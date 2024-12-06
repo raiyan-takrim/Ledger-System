bfb1a6005e0951dbd708b7210262e2c3314433a3
deenafiyaa <nurdeenasofiyazamzuri@gmail.com>
2024-12-06 13:13:37 +0800

    Added User Profile to Screens Class

--- src/Screens.java
+++ src/Screens.java
@@ -14,6 +14,7 @@ public class Screens {
             if (choice == 1) {
                 if(UserActions.login()) {
                     System.out.println("Login Successful!!!");
+                    UserProfile();
                     break;
                 }
             } else if (choice == 2) {
@@ -25,5 +26,50 @@ public class Screens {
                 System.out.println("Invalid Input!\n");
         }
         input.close();
+    }
+
+    public static void UserProfile(){
+        Scanner input = new Scanner(System.in);
+
+            System.out.println("\n== Welcome, "+" ==");
+            System.out.println("Balance: ");
+            System.out.println("Savings: ");
+            System.out.println("Loan: ");
+    
+            System.out.println("\n== Transaction ==");
+            System.out.println("1.Debit");
+            System.out.println("2.Credit");
+            System.out.println("3.History");
+            System.out.println("4.Savings");
+            System.out.println("5.Credit Loan");
+            System.out.println("6.Deposit Interest Predictor");
+            System.out.println("7.Logout");
+            System.out.print(">");
+            int choice = input.nextInt();
+
+            switch(choice){
+                case 1:
+                    TransactionMethods.debit();
+                    break;
+                case 2:
+                    TransactionMethods.credit();
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    TransactionMethods.savings();
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    System.out.println("Thank you for using Ledger System");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+                
+            }
     }
 }
